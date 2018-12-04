d3bd63f866d95d42dbdc2027181b4ebb3adfac5f
gaohuihui <1254746382@qq.com>
2018-12-04 13:46:17 +0800

    商品种类

--- sells/models.py
+++ sells/models.py
@@ -20,4 +20,40 @@ class User(models.Model):
         return self.stuid
 
     class Meta:
-       ordering = ('-created',) 
+       ordering = ('-created',)
+       verbose_name = 'User'
+       verbose_name_plural = 'Users'
+       
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    stuid=models.ForeignKey(User, related_name='User', on_delete=models.CASCADE)
+    gold = models.CharField(max_length=100,db_index=True)
+    tel= models.CharField(max_length=11,db_index=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
