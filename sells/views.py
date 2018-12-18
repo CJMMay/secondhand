c1a384f166bf8e86cd833b92348a8016fec2e835
LuoJing <397411535@qq.com>
2018-12-18 19:49:36 +0800

    cart

--- sells/views.py
+++ sells/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, render_to_response
+from login_res.models import User
 from .models import Category, Product
 from django import forms
 from django.http import HttpResponseRedirect
 from cart.forms import CartAddProductForm
-
+#from django.http import HttpResponseRedirect
 # Create your views here.
 
 # 根据品类显示商品
@@ -20,7 +20,33 @@ def product_list(request, category_slug=None):
 
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(Product, id=id, slug=slug, availbable=True)
-    cart_product_form = CartAddProductForm()
-    #return render(request, 'sells/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
     return render(request, 'detailinfo.html', {'product': product})
+
+#发布闲置
+#显示发布页面
+def publish(request):
+    try:
+        del request.session['error']
+    except:
+        return render(request,'publish.html')
+    return render(request,'publish.html')
+
+#发布操作
+def do_publish(request):
+     stuid = request.session['stuid']
+     users=User.objects.all()
+     for i in users:
+         if i.stuid==stuid:
+             id=i
+     name=request.POST.get('name')
+     tel=request.POST.get('tel')
+     description=request.POST.get('description')
+     price=request.POST.get('price')
+     gold=request.POST.get('gold')
+     category=request.POST.get('category')
+     cat=Category.objects.get(pk=category)
+     img =request.FILES.get('img')
+     Product.objects.create(name=name,description=description,price=price,gold=gold,category=cat,tel=tel,stuid=id,image=img)
+     return render_to_response('sells/product/home.html', {'stuid': stuid})
+ #return render(request, 'sells/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
