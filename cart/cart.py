7f3bf93cc1a5c63a3c37a8738f199d750d9d6ccb
LuoJing <397411535@qq.com>
2018-12-19 13:07:50 +0800

    cart

--- cart/cart.py
+++ cart/cart.py
@@ -17,17 +17,17 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart =cart
         
-    def add(self, product,quantity=1, update_quantity=False):
+    def add(self, product):
 #        """
 #        向购物车中增加商品或者更新购物车中的数量
 #        """
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
-        if update_quantity:
-            self.cart[product_id]['quantity'] = quantity
-        else:
-            self.cart[product_id]['quantity'] += quantity
+#        if update_quantity:
+#            self.cart[product_id]['quantity'] = quantity
+#        else:
+#            self.cart[product_id]['quantity'] += quantity
         self.save()
     
 
