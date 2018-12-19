3ae6f1e03ec9450036236202da023d9e57d4f0b9
LuoJing <397411535@qq.com>
2018-12-19 11:23:51 +0800

    cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#from decimal import Decimal
+from decimal import Decimal
 from django.conf import settings
 from sells.models import Product
 
@@ -17,18 +17,19 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart =cart
         
-    def add(self, product):
-        """
-        向购物车中增加商品或者更新购物车中的数量
-        """
+    def add(self, product,quantity=1, update_quantity=False):
+#        """
+#        向购物车中增加商品或者更新购物车中的数量
+#        """
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
-#        if update_quantity:
-#            self.cart[product_id]['quantity'] = quantity
-#        else:
-#            self.cart[product_id]['quantity'] += quantity
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
         self.save()
+    
 
     def save(self):
     # 设置session.modified的值为True，中间件在看到这个属性的时候，就会保存session
@@ -55,10 +56,10 @@ class Cart:
         for product in products:
             cart[str(product.id)]['product'] = product
 
-#        for item in cart.values():
-#            item['price'] = Decimal(item['price'])
-#            item['total_price'] = item['price'] * item['quantity']
-#            yield item
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
 #            
 #    def __len__(self):
 #        """
--- cart/views.py
+++ cart/views.py
@@ -13,7 +13,7 @@ def cart_add(request, product_id):
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
-        cart.add(product=product)
+        cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
     return redirect('cart:cart_detail')
 
 def cart_remove(request, product_id):
@@ -24,4 +24,6 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
     cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'update': True})
     return render(request, 'cart/detail.html', {'cart': cart})
